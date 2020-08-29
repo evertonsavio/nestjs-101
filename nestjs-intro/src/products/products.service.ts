9e3f96b8b22eeaf776ee1f320da5909d75768a87
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-29 10:52:47 -0300

    :sparkles: fetch products

--- nestjs-intro/src/products/products.service.ts
+++ nestjs-intro/src/products/products.service.ts
@@ -3,12 +3,17 @@ import { Product } from './products.model';
 
 @Injectable()
 export class ProductService {
-  products: Product[] = [];
+  private products: Product[] = []; // LEMBRAR DE COLOCAR PRIVATE PRA SO O METODO ACESSAR
+
   insertProduct(title: string, desc: string, price: number): string {
     const prodId = new Date().toString(); //dummy id
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
 
     return prodId;
+  }
+
+  getProducts() {
+    return [...this.products];
   }
 }
